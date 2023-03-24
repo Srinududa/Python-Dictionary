b23f056d5ef39a14c7ac9f9cc9d12bd055640037
Srinu Duda <128722536+Srinududa@users.noreply.github.com>
2023-03-24 13:02:45 +0530

    Click here to open the file

---
+++ dictionary practice.py
@@ -0,0 +1,183 @@
+# practices methods of dictonary
+
+# s={'amma': 'nanna', 'both':1, 'annayya':3}
+# for i in s:
+#     print (i)
+
+s={'amma': 'nanna', 'both':1, 'annayya':3}
+# s.update({'joining':'today'})
+# print (s)
+s['amma']='ammamma'
+print (s)
+
+
+
+
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+{'amma': 'nanna', 'both': 1, 'annayya': 3}
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+<class 'dict'>
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+amma
+both   
+annayya
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 9
+    print(if)
+          ^^
+SyntaxError: invalid syntax
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 9, in <module>
+    print(i)
+          ^
+NameError: name 'i' is not defined. Did you mean: 'id'?
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+amma
+both
+annayya
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 9, in <module>
+    print(k.items())
+          ^^^^^^^
+AttributeError: 'str' object has no attribute 'items'
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8, in <module>
+    print (s,get('nanna'))
+             ^^^
+NameError: name 'get' is not defined. Did you mean: 'set'?
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+None
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+nanna
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+1
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8, in <module>
+    print (s.key('nanna'))
+           ^^^^^
+AttributeError: 'dict' object has no attribute 'key'. Did you mean: 'keys'?
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8, in <module>
+    print (s.keys('nanna'))
+           ^^^^^^^^^^^^^^^
+TypeError: dict.keys() takes no arguments (1 given)
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+dict_keys(['amma', 'both', 'annayya'])
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8, in <module>
+    print (s.key())
+           ^^^^^
+AttributeError: 'dict' object has no attribute 'key'. Did you mean: 'keys'?
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+dict_keys(['amma', 'both', 'annayya'])
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8, in <module>
+    print (s.keys('nanna'))
+           ^^^^^^^^^^^^^^^
+TypeError: dict.keys() takes no arguments (1 given)
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8, in <module>
+    print (s.keys('amma'))
+           ^^^^^^^^^^^^^^
+TypeError: dict.keys() takes no arguments (1 given)
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8, in <module>
+    print (s.keys('3'))
+           ^^^^^^^^^^^
+TypeError: dict.keys() takes no arguments (1 given)
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8, in <module>
+    print (s.keys(3))
+           ^^^^^^^^^
+TypeError: dict.keys() takes no arguments (1 given)
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+dict_keys(['amma', 'both', 'annayya'])
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+nanna
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+dict_values(['nanna', 1, 3])
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8, in <module>
+    print (s.values('amma'))
+           ^^^^^^^^^^^^^^^^
+TypeError: dict.values() takes no arguments (1 given)
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8, in <module>
+    print (s.values(1))
+           ^^^^^^^^^^^
+TypeError: dict.values() takes no arguments (1 given)
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 9
+    print (s.items(c))
+    ^
+IndentationError: expected an indented block after 'if' statement on line 8
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8, in <module>
+    if c in s:
+       ^
+NameError: name 'c' is not defined
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 9, in <module>
+    if (c==s):
+        ^
+NameError: name 'c' is not defined
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 9, in <module>
+    if c==s:
+       ^
+NameError: name 'c' is not defined
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+dict_items([('amma', 'nanna'), ('both', 1), ('annayya', 3)])
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8, in <module>
+    c=s.items ('' 'sep:')
+      ^^^^^^^^^^^^^^^^^^^
+TypeError: dict.items() takes no arguments (1 given)
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 9, in <module>
+    print (c('' 'sep:'))
+           ^^^^^^^^^^^^
+TypeError: 'dict_items' object is not callable
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8
+    for (a,b in s.items()):
+                          ^
+SyntaxError: invalid syntax
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+amma:nanna
+both:1
+annayya:3
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8, in <module>
+    s.upadate({'joining':'today'})
+    ^^^^^^^^^
+AttributeError: 'dict' object has no attribute 'upadate'. Did you mean: 'update'?
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+Traceback (most recent call last):
+  File "c:\Users\SRINU DUDA\Desktop\python folder\dictionary practice.py", line 8, in <module>
+    s.upadate({'joining':'today'})
+    ^^^^^^^^^
+AttributeError: 'dict' object has no attribute 'upadate'. Did you mean: 'update'?
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+{'amma': 'nanna', 'both': 1, 'annayya': 3, 'joining': 'today'}
+PS C:\Users\SRINU DUDA\Desktop\python folder> & "C:/Users/SRINU DUDA/AppData/Local/Programs/Python/Python311/python.exe" "c:/Users/SRINU DUDA/Desktop/python folder/dictionary practice.py"
+{'amma': 'ammamma', 'both': 1, 'annayya': 3}
+PS C:\Users\SRINU DUDA\Desktop\python folder>    
